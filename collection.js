139e74777c5c20e81f32e7c762929436c52f6b33
sudharsan136 <sudharsan1364@gmail.com>
2025-07-28 10:02:13 +0530

    updated nostra

--- collection.js
+++ collection.js
@@ -6,6 +6,9 @@ var right = document.getElementById("right")
 var image = document.getElementById("img")
 var pop = document.getElementById("pop")
 var btn = document.getElementById("btn")
+summerimg = document.getElementById("summer")
+winteimg = document.getElementById("winter")
+beachimg = document.getElementById("beach")
 
 side.addEventListener("click", function () {
     nav.style.left = 0
@@ -16,12 +19,17 @@ clos.addEventListener("click", function () {
     nav.style.left = "-50%"
 })
 btn.addEventListener("click", function () {
-    pop.style.display="none"
+    pop.style.display = "none"
 })
 
 //here for collectopns
 
 var ocassion = document.getElementsByName("ocasion")
-     ocassion[0].addEventListener("click",function(){
-        console.log(ocassion[0].value)
-     })
+ocassion[0].addEventListener("click", function () {
+    if (ocassion[0].checked) {
+        winteimg.style.display = "none"
+        beachimg.style.display = "none"
+    }
+  
+}
+ )
